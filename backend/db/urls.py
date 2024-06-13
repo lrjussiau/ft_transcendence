eed3c79b5516779ab2eda1584e7e7f7431d5a4d8
lrjussiau <lr.jussiaume@gmail.com>
2024-06-13 19:02:55 +0200

    almost connect

--- backend/db/urls.py
+++ backend/db/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import GenericModelView
 
 urlpatterns = [
-    path('api/db/<str:model_name>/', GenericModelView.as_view(), name='generic_model_view'),
+    path('<str:model_name>/', GenericModelView.as_view(), name='generic_model_view'),
 ]
