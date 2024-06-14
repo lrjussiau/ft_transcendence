82c709c180b8ccd9d6cb2b1cd2b10cbaf6c04efc
Valentin Vuadens <vvuadens@c3r6s1.42lausanne.ch>
2024-06-14 08:46:06 +0200

    get/post data to pongdatabase ok v1

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -6,15 +6,32 @@ from django.contrib.auth import authenticate
 from rest_framework import status, generics
 from .models import CustomUser
 from .serializers import CustomUserSerializer  # Ensure this import path is correct
+import requests
+
+def authent(username, password):
+    base_url = 'http://localhost:8000/api/db/User/'
+    params = {
+        'fields': 'username',
+        'filter__password_hash': password
+    }
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    print(f"Here is the data received {data}")
+    if data:
+        if data[0]['username'] == username:
+            print("We got:",data[0]['username'])
+            return True
+        return False
 
 class AuthView(APIView):
+
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key})
+        if authent(username=username, password=password):
+            return Response({"message": "Authentication successed welcome"})
+            #token, created = Token.objects.get_or_create(user=user)
+            #return Response({'token': token.key})
         return Response({"message": "Authentication failed"}, status=status.HTTP_401_UNAUTHORIZED)
 
 class UserListCreate(generics.ListCreateAPIView):
