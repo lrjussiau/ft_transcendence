329d357c37652d36b6914b0732eda68bdac61dd8
lrjussiau <lr.jussiaume@gmail.com>
2024-06-19 11:30:08 +0200

    Auth/register UP

--- frontend/website/static/script/register.js
+++ frontend/website/static/script/register.js
@@ -1,57 +1,4 @@
-// document.getElementById('register-form').addEventListener('submit', function(event) {
-//     event.preventDefault(); // Prevent the default form submit
-
-//     // Gather data from the form
-//     var username = document.getElementById('username').value;
-//     var email = document.getElementById('email').value;
-//     var password = document.getElementById('password').value;
-
-//     // Prepare the data to send in the POST request
-//     var data = {
-//         username: username,
-//         email: email,
-//         password: password
-//     };
-
-//     // Use fetch API to send the POST request
-//     fetch('http://localhost:8000/api/authentication/register/', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)  // Convert the JavaScript object to a JSON string
-//     })
-//     .then(response => {
-//         if (!response.ok) {
-//             return response.json().then(data => {
-//                 throw new Error(data.error || 'Something went wrong');
-//             });
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         if (data.id) {
-//             console.log('Success:', data);
-//             alert('User created successfully!');
-//         }
-//     })
-//     .catch((error) => {
-//         console.error('Error:', error);
-//         alert('Failed to create user: ' + error.message);
-//     });
-// });
-
-
 console.log("register.js loaded");
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM fully loaded and parsed - register.js");
-  
-    // Set up the register form if the initial route is the register page
-    if (getCurrentRoute() === 'register') {
-      setupRegisterForm();
-    }
-  });
 
 // Function to set up the register form
 function setupRegisterForm() {
@@ -96,3 +43,12 @@ function setupRegisterForm() {
     console.error("Registration form not found");
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed - register.js");
+
+  // Set up the register form if the initial route is the register page
+  if (getCurrentRoute() === 'register') {
+    setupRegisterForm();
+  }
+});
