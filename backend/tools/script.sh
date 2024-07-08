889c05f5ccb9100987037ec3842fc4f01ffc9440
🏓Multi-Pong-Linux-Bino🏓 <smonte-e@c3r5s5.42lausanne.ch>
2024-07-08 14:37:29 +0200

    patch auto-load image

--- backend/tools/script.sh
+++ backend/tools/script.sh
@@ -5,14 +5,10 @@ IMAGE_URL1="https://cdn.intra.42.fr/users/691f632ce11bd5527255a3dc742d2db5/vrey.
 IMAGE_URL2="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/NES-ROB.jpg/330px-NES-ROB.jpg"
 
 # Nom du fichier de destination
-DESTINATION_FILE1="/app/media/avatars/default_avatar.png"
-DESTINATION_FILE2="/app/media/avatars/ai.png"
-
-# Créez le répertoire si nécessaire
-mkdir -p $(dirname ${DESTINATION_FILE})
+DESTINATION_FILE1="default_avatar.png"
+DESTINATION_FILE2="ai.png"
 
 # Téléchargez l'image
 curl -o ${DESTINATION_FILE1} ${IMAGE_URL1}
 curl -o ${DESTINATION_FILE2} ${IMAGE_URL2}
 
-exec "$@"
